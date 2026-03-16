a215078c8aa78b7ed56384dbfa56d5f53bfbf990
Kaif <kaif9999@Kaifs-MacBook-Air.local>
2026-03-17 00:22:20 +0530

    feat: updated pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,6 +2,8 @@
 
 import Pricing from "@/components/pricing"
 
+export const dynamic = "force-dynamic"
+
 export default function PricingPage() {
   return (
     <div className="min-h-screen bg-black">
